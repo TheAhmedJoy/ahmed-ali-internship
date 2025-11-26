592220b49d30582ccd153ea66cf87e3fb3d76e28
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-25 16:07:06 -0800

    NewItemsTile UI component, caluculating item expiration via interval

--- src/components/UI/NewItemsTile.jsx
+++ src/components/UI/NewItemsTile.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NewItemsTile({ authorId, authorImage, nftImage, nftId, title, price, likes, expiryDate }) {
-  return (
-    <div>NewItemsTile</div>
-  )
+
+    const [secondsLeft, setSecondsLeft] = useState()
+    const [minutesLeft, setMinutesLeft] = useState()
+    const [hoursLeft, setHoursLeft] = useState()
+
+    useEffect(() => {
+        if (expiryDate) {
+            let interval = setInterval(calculateExpiration, 1000)
+            return () => clearInterval(interval)
+        }
+    }, [])
+
+    function calculateExpiration() {
+        const milliSecondsLeft = expiryDate - Date.now()
+        const seconds = milliSecondsLeft / 1000
+        const minutes = seconds / 60
+        const hours =  minutes / 60
+        
+        setSecondsLeft(Math.floor(seconds) % 60)
+        setMinutesLeft(Math.floor(minutes) % 60)
+        setHoursLeft(Math.floor(hours))
+    }
+
+    return (
+        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" style={{ width: "100%", maxWidth: "100%", padding: "0" }} >
+            <div className="nft__item">
+                <div className="author_list_pp">
+                    <Link to={`/author/`} >
+                        <img className="lazy" src={authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                    </Link>
+                </div>
+                {expiryDate && (
+                    <div className="de_countdown">
+                        {hoursLeft}h {minutesLeft}m {secondsLeft}s
+                    </div>
+                )}
+                <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                            <button>Buy Now</button>
+                            <div className="nft__item_share">
+                                <h4>Share</h4>
+                                <a href="" target="_blank" rel="noreferrer">
+                                    <i className="fa fa-facebook fa-lg"></i>
+                                </a>
+                                <a href="" target="_blank" rel="noreferrer">
+                                    <i className="fa fa-twitter fa-lg"></i>
+                                </a>
+                                <a href="">
+                                    <i className="fa fa-envelope fa-lg"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <Link to={`/item-details/`}>
+                        <img src={nftImage} className="lazy nft__item_preview" alt="" />
+                    </Link>
+                </div>
+                <div className="nft__item_info">
+                    <Link to={`/item-details/`}>
+                        <h4>{title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{price}</div>
+                    <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{likes}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
