97258e260259702a5f11f620eed5f4e7c06e65a5
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-28 16:45:32 -0800

    Updated links to utilize author pages

--- src/components/UI/NewItemsTile.jsx
+++ src/components/UI/NewItemsTile.jsx
@@ -10,7 +10,7 @@ export default function NewItemsTile({ authorId, authorImage, nftImage, nftId, t
         <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" style={carouselStyling}>
             <div className="nft__item">
                 <div className="author_list_pp">
-                    <Link to={`/author/`} >
+                    <Link to={`/author/${authorId}`} >
                         <img className="lazy" src={authorImage} alt="" />
                         <i className="fa fa-check"></i>
                     </Link>
@@ -36,12 +36,12 @@ export default function NewItemsTile({ authorId, authorImage, nftImage, nftId, t
                             </div>
                         </div>
                     </div>
-                    <Link to={`/item-details/`}>
+                    <Link to={`/item-details/${nftId}`}>
                         <img src={nftImage} className="lazy nft__item_preview" alt="" />
                     </Link>
                 </div>
                 <div className="nft__item_info">
-                    <Link to={`/item-details/`}>
+                    <Link to={`/item-details/${nftId}`}>
                         <h4>{title}</h4>
                     </Link>
                     <div className="nft__item_price">{price}</div>
